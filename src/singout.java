eb415107c875f8ec40779ae3dec70424a13e530e
root <root@ip-172-31-23-195.us-east-2.compute.internal>
2018-03-29 16:34:36 +0000

    wnhjdhs'

---
+++ src/singout.java
@@ -0,0 +1,10 @@
+public class singout {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
+}
+
+dwnjkdk
+dsjdjls
+dwjkd'
